8cbe0721523da0bcb658580834a6fba6fcfa31c1
WarlordShado <148493542+WarlordShado@users.noreply.github.com>
2024-11-10 10:56:56 -0500

    Added Icon File

--- sprites.lua
+++ sprites.lua
@@ -1,6 +1,13 @@
-SMODS.Atlas {
+SMODS.Atlas({
+	key = "modicon",
+	path = "icon.png",
+	px = 32,
+	py = 32
+}):register()
+
+SMODS.Atlas({
 	key = "JoJokers",
 	path = "JoJokers.png",
 	px = 71,
-	py = 95,
-}:register()
+	py = 95
+}):register()
