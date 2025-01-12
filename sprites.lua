c61701330f72c7535bae56038b8059e1e2e0cae5
WarlordShado <148493542+WarlordShado@users.noreply.github.com>
2025-01-12 17:29:55 -0500

    Fixed Major Bug
    Fixed Priority inside lovely.toml. It was patching something that SMODS was trying to patch and completly disabled scoring of cards

--- sprites.lua
+++ sprites.lua
@@ -3,14 +3,14 @@ SMODS.Atlas({
 	path = "icon.png",
 	px = 32,
 	py = 32
-}):register()
+})
 
 SMODS.Atlas({
 	key = "JoJokers",
 	path = "JoJokers.png",
 	px = 71,
 	py = 95
-}):register()
+})
 
 SMODS.Atlas({
     key = "JoJokers7",
